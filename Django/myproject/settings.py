88952ebe91ca28241cdc98a42a1d9ff06be8e093
DevEris <mhmdharshadi2008@gmail.com>
2025-07-02 06:50:18 +0330

    Add render FrontEnd To CORS

--- Django/myproject/settings.py
+++ Django/myproject/settings.py
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://fitness-calender.onrender.com",  # ← آدرس فرانت‌اند در Render
 ]
 
 from datetime import timedelta
